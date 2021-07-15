154975d1d7665b800eb71f4e8ab1fb9e5726d508
Mano <vmano@ymail.com>
2021-07-15 15:04:42 +0000

    end of exercise checkpoint

--- src/backend/events.js
+++ src/backend/events.js
@@ -2,7 +2,7 @@ module.exports = [
     {
       id: 1,
       title: 'Docker Workshop',
-      detail: 'Linuxing in Lonodon ',
+      detail: 'Linuxing in Brooklyn',
       date: '2017-11-21'
     },
     {
@@ -15,5 +15,15 @@ module.exports = [
       id: 3,
       title: 'Docker London',
       date: '2017-11-13'
+    },
+{
+      id: 4,
+      title: 'Docker Manos docker changes with npm',
+      date: '2021-07-15'
+    },
+    {
+      id: 5,
+      title: 'Docker Manos docker changes with nodemon',
+      date: '2021-07-15'
     }
   ];
