e576e031f816cded867365b5e5c76fa51121b767
Mano <vmano@ymail.com>
2021-07-16 20:53:19 +0000

    tomatch resist for fail

--- api/integration/api-spec.js
+++ api/integration/api-spec.js
@@ -5,7 +5,7 @@ describe("When testing 'api/products/1'", function(){
     it("should respond with entry for 'resistor'", function(done) {
         request(base_url + 'api/products/1', function(error, response, body){
             product = JSON.parse(body)
-            expect(product["description"]).toMatch('resistor');
+            expect(product["description"]).toMatch('resist');
             done();
         });
     });
